48b326fc184554684ead54d5233c7a498a158440
BrCOS <cosbrnta@gmail.com>
2024-10-31 00:42:09 -0300

    refactor: altera o nome da chave do dicionario

--- data/dicionarioGrafo.py
+++ data/dicionarioGrafo.py
@@ -1,33 +1,33 @@
 grafo = {
     # Centro Belém
-    'Belem_1': {'Belem_2': 85, 'Belem_3': 95},
-    'Belem_2': {'Belem_1': 85, 'Belem_3': 15, 'Belem_4': 70},
-    'Belem_3': {'Belem_1': 95, 'Belem_2': 15, 'Belem_5': 55},
-    'Belem_4': {'Belem_2': 70, 'Belem_5': 90, 'Belem_6': 100},
-    'Belem_5': {'Belem_3': 55, 'Belem_4': 90, 'Belem_6': 10},
-    'Belem_6': {'Belem_4': 100, 'Belem_5': 10},
+    'Belem_Centro': {'Belem_1': 85, 'Belem_2': 95},
+    'Belem_1': {'Belem_Centro': 85, 'Belem_2': 15, 'Belem_3': 70},
+    'Belem_2': {'Belem_Centro': 95, 'Belem_1': 15, 'Belem_4': 55},
+    'Belem_3': {'Belem_1': 70, 'Belem_4': 90, 'Belem_5': 100},
+    'Belem_4': {'Belem_2': 55, 'Belem_3': 90, 'Belem_5': 10},
+    'Belem_5': {'Belem_3': 100, 'Belem_4': 10},
 
     # Centro Recife
-    'Recife_1': {'Recife_2': 60, 'Recife_3': 80},
-    'Recife_2': {'Recife_1': 60, 'Recife_3': 25, 'Recife_4': 95},
-    'Recife_3': {'Recife_1': 80, 'Recife_2': 25, 'Recife_5': 75},
-    'Recife_4': {'Recife_2': 95, 'Recife_5': 65, 'Recife_6': 85},
-    'Recife_5': {'Recife_3': 75, 'Recife_4': 65, 'Recife_6': 20},
-    'Recife_6': {'Recife_4': 85, 'Recife_5': 20},
+    'Recife_Centro': {'Recife_1': 60, 'Recife_2': 80},
+    'Recife_1': {'Recife_Centro': 60, 'Recife_2': 25, 'Recife_3': 95},
+    'Recife_2': {'Recife_Centro': 80, 'Recife_1': 25, 'Recife_4': 75},
+    'Recife_3': {'Recife_1': 95, 'Recife_4': 65, 'Recife_5': 85},
+    'Recife_4': {'Recife_2': 75, 'Recife_3': 65, 'Recife_5': 20},
+    'Recife_5': {'Recife_3': 85, 'Recife_4': 20},
 
     # Centro São Paulo
-    'SaoPaulo_1': {'SaoPaulo_2': 10, 'SaoPaulo_3': 15, 'Curitiba_1': 90},
-    'SaoPaulo_2': {'SaoPaulo_1': 10, 'SaoPaulo_3': 8, 'SaoPaulo_4': 20},
-    'SaoPaulo_3': {'SaoPaulo_1': 15, 'SaoPaulo_2': 8, 'SaoPaulo_5': 30},
-    'SaoPaulo_4': {'SaoPaulo_2': 20, 'SaoPaulo_5': 5, 'SaoPaulo_6': 45},
-    'SaoPaulo_5': {'SaoPaulo_3': 30, 'SaoPaulo_4': 5, 'SaoPaulo_6': 7},
-    'SaoPaulo_6': {'SaoPaulo_4': 45, 'SaoPaulo_5': 7, 'Curitiba_2': 70},
+    'SaoPaulo_Centro': {'SaoPaulo_1': 10, 'SaoPaulo_2': 15, 'Curitiba_Centro': 90},
+    'SaoPaulo_1': {'SaoPaulo_Centro': 10, 'SaoPaulo_2': 8, 'SaoPaulo_3': 20},
+    'SaoPaulo_2': {'SaoPaulo_Centro': 15, 'SaoPaulo_1': 8, 'SaoPaulo_4': 30},
+    'SaoPaulo_3': {'SaoPaulo_1': 20, 'SaoPaulo_4': 5, 'SaoPaulo_5': 45},
+    'SaoPaulo_4': {'SaoPaulo_2': 30, 'SaoPaulo_3': 5, 'SaoPaulo_5': 7},
+    'SaoPaulo_5': {'SaoPaulo_3': 45, 'SaoPaulo_4': 7, 'Curitiba_1': 70},
 
     # Centro Curitiba
-    'Curitiba_1': {'Curitiba_2': 5, 'Curitiba_3': 15, 'SaoPaulo_1': 90},
-    'Curitiba_2': {'Curitiba_1': 5, 'Curitiba_3': 10, 'Curitiba_4': 40, 'SaoPaulo_6': 70},
-    'Curitiba_3': {'Curitiba_1': 15, 'Curitiba_2': 10, 'Curitiba_5': 6},
-    'Curitiba_4': {'Curitiba_2': 40, 'Curitiba_5': 9, 'Curitiba_6': 20},
-    'Curitiba_5': {'Curitiba_3': 6, 'Curitiba_4': 9, 'Curitiba_6': 10},
-    'Curitiba_6': {'Curitiba_4': 20, 'Curitiba_5': 10}
+    'Curitiba_Centro': {'Curitiba_1': 5, 'Curitiba_2': 15, 'SaoPaulo_Centro': 90},
+    'Curitiba_1': {'Curitiba_Centro': 5, 'Curitiba_2': 10, 'Curitiba_3': 40, 'SaoPaulo_5': 70},
+    'Curitiba_2': {'Curitiba_Centro': 15, 'Curitiba_1': 10, 'Curitiba_4': 6},
+    'Curitiba_3': {'Curitiba_1': 40, 'Curitiba_4': 9, 'Curitiba_5': 20},
+    'Curitiba_4': {'Curitiba_2': 6, 'Curitiba_3': 9, 'Curitiba_5': 10},
+    'Curitiba_5': {'Curitiba_3': 20, 'Curitiba_4': 10}
 }
