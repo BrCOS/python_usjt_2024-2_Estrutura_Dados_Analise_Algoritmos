102897cc480a4c65a8de9407731ef5e8a5132e1a
BrCOS <cosbrnta@gmail.com>
2024-11-09 04:14:35 -0300

    feat: agrupa as cidades por centro

--- src/models/rotas.py
+++ src/models/rotas.py
@@ -1,6 +1,6 @@
-from .dijkstra import dijkstra
+from src.models.dijkstra import dijkstra
 
-def rotas(grafo, cidadeDestino):
+def otimizarRotas(grafo, cidadeDestino):
     centros = {
         'Belem_Centro': 'Belem',
         'Recife_Centro': 'Recife',
@@ -8,32 +8,35 @@ def rotas(grafo, cidadeDestino):
         'Curitiba_Centro': 'Curitiba'
     }
 
-    centroGrupo = {}
+    #agrupa os centros pelas cidades passadas
+    #centro = chave e lista vazia (cidades) = valores
+    centroGrupo = {centro: [] for centro in centros}
 
-    for centro in centros:
-        centroGrupo[centro] = []
-
+    #passa as cidades para a [] do dicionario centroGrupo
     for cidade in cidadeDestino:
-        for centro, prefixo in centros.items():
-            if cidade.startswith(prefixo):
-                centroGrupo[centro].append(cidade)
-                break
+        for centro, prefixoCentro in centros.items():
+            if cidade.startswith(prefixoCentro):#verifica o prefixo das cidades
+                centroGrupo[centro].append(cidade)#agrupa as cidades por centro
 
-    rota = {}
+    rota = {}#dicionario p/ retornar o caminho final
 
+    #itera sobre o dicionario centroGrupo -> centro (chave), cidade[] (valor)
     for centro, cidadeGrupo in centroGrupo.items():
-        caminhosDijkstra = dijkstra(grafo, centro)
+        caminhosDijkstra = dijkstra(grafo, centro)#calcula o menor caminho com base no centro e no grafo
 
-        caminhoGrupo = []
-        
-        cidadeIncluida = set()
+        caminhoGrupo = []#caminho final dos centros
+        cidadeVisitada = set()#set p/ n ter cidades duplicadas (ja visitadas)
 
+        #uso do alg. de dijkstra
+        #p/ cada cidade da lista com base no centro
         for cidade in cidadeGrupo:
-            for ponto in caminhosDijkstra[cidade]:
-                if ponto not in cidadeIncluida:
-                    caminhoGrupo.append(ponto)
-                    cidadeIncluida.add(ponto)
+            for ponto in caminhosDijkstra[cidade]:#percorre o menor caminho
+                if ponto not in cidadeVisitada:#ponto == aresta
+                    caminhoGrupo.append(ponto)#adiciona o caminho final na lista
+                    cidadeVisitada.add(ponto)#adiciona as cidades que ja visitadas
 
-        rota[centro] = caminhoGrupo
+        #caminho p/ cada centro
+        #centro (chave) e cidades [] (valores)
+        rota[centro] = caminhoGrupo #rota[centro] += caminhoGrupo -> duplica as cidades (valores)
 
     return rota
