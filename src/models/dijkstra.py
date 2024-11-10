2cdeb6e563da64b3e1e3e5356320f814776e384e
BrCOS <cosbrnta@gmail.com>
2024-11-10 04:46:40 -0300

    feat: atualiza o retorno somente com os caminhos

--- src/models/dijkstra.py
+++ src/models/dijkstra.py
@@ -1,25 +1,32 @@
 import heapq
 
 def dijkstra(grafo, inicio):
-    distancias = {cidade: float('inf') for cidade in grafo}
-    distancias[inicio] = 0
-    rota = {cidade: [] for cidade in grafo}
-    rota[inicio] = [inicio]
-    
-    filaPrioridade = [(0, inicio)]
+    distancias = {cidade: float('inf') for cidade in grafo}#cidades com o peso infinito
+    distancias[inicio] = 0#inicio definido como 0 (cidade de partida - centro)
 
-    while filaPrioridade:
-        distanciaAtual, cidadeAtual = heapq.heappop(filaPrioridade)
+    caminho = {cidade: [] for cidade in grafo}#[] recebe o menor caminho (return)
+    caminho[inicio] = [inicio]#define o caminho 0 (centro) ate ele mesmo (ponto inicial do algoritmo)
 
+    filaPrioridade = [(0, inicio)]#define o centro como prioridade (distancia (peso) 0)
+
+    #continuar enquanto tiver elementos na lista []
+    while filaPrioridade:#while pq a [] nao tem tamanho fixo
+        #heapq p/ processar a menor distancia (peso) das cidades
+        #heappop p/ remover as cidades ja processadas da [] (com o menor caminho) ate acabar as cidades
+        distanciaAtual, cidadeAtual = heapq.heappop(filaPrioridade)#passa o peso e a cidade com menor distancia pro dicionario
+
+        #ignora caminhos mais longos
         if distanciaAtual > distancias[cidadeAtual]:
             continue
-
+        
+        #acesso aos vizinhos e seus pesos da cidade atual
         for vizinho, peso in grafo[cidadeAtual].items():
             distancia = distanciaAtual + peso
 
+            #compara o peso (distancia) com a atual e atualiza a []
             if distancia < distancias[vizinho]:
-                distancias[vizinho] = distancia
-                rota[vizinho] = rota[cidadeAtual] + [vizinho]
-                heapq.heappush(filaPrioridade, (distancia, vizinho))
-    
-    return rota
+                distancias[vizinho] = distancia#atualiza a cidade com o menor caminho encontrado
+                caminho[vizinho] = caminho[cidadeAtual] + [vizinho]#passa o menor caminho seguindo do vizinho
+                heapq.heappush(filaPrioridade, (distancia, vizinho))#passa o caminho mais curto p/ while
+
+    return caminho
